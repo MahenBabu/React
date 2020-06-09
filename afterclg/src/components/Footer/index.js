aa99dee246ef81eb603f7033f955d02c0529d94c
MahenBabu <mahen.babu1990@gmail.com>
2020-06-09 21:34:02 +0530

    small changes

--- afterclg/src/components/Footer/index.js
+++ afterclg/src/components/Footer/index.js
@@ -3,11 +3,7 @@ import './styles.scss'
 function Footer() {
     return (
         <div>
-<<<<<<< HEAD
-            <h1>Footer Section Added.</h1>
-=======
             <h1>Footer</h1>
->>>>>>> 1fac6dc90cae541ee2710e616bb84c9166d8b889
         </div>
     )
 }
